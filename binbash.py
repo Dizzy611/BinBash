18e12d0c3c4e2609ea1cb87b48e98a626cc78145
Dylan Morrison <insidious622@ps8983.dreamhostps.com>
2010-06-10 03:16:58 -0700

    Added licensing.

--- binbash.py
+++ binbash.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# BinBash, IRC quote bot.
+# BinBash is Copyright 2010 Dylan Morrison
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # Edit these
 SERVER="irc.foo.org" # Hostname of the server the bot is connecting to
 PORT=6667 # Port the bot is connecting to
